43b28ba034cf2ada6c477a327a1149c32b7e5f3f
LionScotty <2236000574@utna.edu.mx>
2025-06-26 19:34:26 -0600

    Configuracion en el archivo servidor para crear una conexion a la base de datos

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app/app');
 const config = require ('./app/config/configuracion');
+<<<<<<< HEAD
 
+=======
+const conexion = require('./app/config/conexion');
+
+conexion.connect()
+>>>>>>> a5f59c0 (Configuracion en el archivo servidor para crear una conexion a la base de datos)
 
 app.listen(config.PORT, () => {
   console.log(`Aplicacion corriendo en puerto ${config.PORT}`);
