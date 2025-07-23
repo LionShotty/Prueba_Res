24b0c2537f7c6920adfef38dca9b346eef2ece8c
LionScotty <2236000574@utna.edu.mx>
2025-07-22 19:24:12 -0600

    Se agregaron las funciones eliminar y actualizar a su vez que se hicieron pruebas de que funcionaran correctamente con la extension ThunderCliente

--- app/controllers/palapaController.js
+++ app/controllers/palapaController.js
@@ -24,7 +24,73 @@ function agregar(req, res){
     })
 }
 
+
+// Cuando se ejecuta la funcion se ejecutan llaves y producto o caracteristica
+
+
+function buscarBebida(req, res, next){
+    let consulta = {}
+    consulta[req.params.key] = req.params.value;
+    console.log(consulta)
+    palapaModel.find(consulta)
+    // En caso sea correcto o hubiese informacion se muestra
+    .then(bebidas =>{
+        if(!bebidas.length) return next()
+            req.body.bebidas = bebidas
+        return next()
+    })
+    // Caso contrario da error
+    .catch (e=>{
+        req.body.e = e
+        return next()
+    })
+}
+
+    function mostrarBebida(req, res){
+      if (req.body.e)
+         return res.status(404).send({message: "Error al consultar la información"})  
+        if(!req.body.bebidas) return res.status(204).send({message:"No hay información que mostrar"})
+            let bebidas = req.body.bebidas
+        return res.status(200).send({bebidas})
+    }
+
+  function eliminarBebida(req,res) {
+    var bebidas = {}
+    bebidas = req.body.bebidas
+    palapaModel.deleteOne(bebidas[0])
+        .then(result => {
+            if (result.deletedCount === 0) {
+                return res.status(404).send({ mensaje: "No se encontró la bebida para eliminar" });
+            }
+            return res.status(200).send({ mensaje: "La información se eliminó con éxito" });
+        })
+        .catch(e => {
+            return res.status(500).send({ mensaje: "Error al eliminar el producto", e });
+        });
+}
+
+
+function actualizarBebida(req, res) {
+  const filtro = { [req.params.key]: req.params.value };
+  const nuevosDatos = req.body;
+
+  if (!Object.keys(nuevosDatos).length)
+    return res.status(400).send({ message: "No hay datos para actualizar." });
+
+  palapaModel.findOneAndUpdate(filtro, nuevosDatos, { new: true })
+    .then(bebida => bebida
+      ? res.status(200).send({ message: "Bebida actualizada", bebida })
+      : res.status(404).send({ message: "Bebida no encontrada" })
+    )
+    .catch(e => res.status(500).send({ message: "Error al actualizar", error: e }));
+}
+
+
 module.exports = {
     buscarTodo,
-    agregar
+    agregar,
+    buscarBebida,
+    mostrarBebida,
+    eliminarBebida,
+    actualizarBebida
 }
--- app/routes/palapaRouter.js
+++ app/routes/palapaRouter.js
@@ -4,5 +4,13 @@ const palapaController = require('../controllers/palapaController');
 
 router.get('/bebidas', palapaController.buscarTodo)
 .post('/bebidas', palapaController.agregar)
+.get('/bebidas/:key/:value',palapaController.buscarBebida,palapaController.mostrarBebida)
+.get('/bebidas/:key/:value',palapaController.buscarBebida,palapaController.mostrarBebida)
+.delete('/bebidas/eliminarone/:key/:value',palapaController.eliminarBebida)
+.delete('/bebidas/eliminar',palapaController.eliminarBebida)
+.put('/bebidas/:key/:value', palapaController.actualizarBebida);
+
+
+
 
 module.exports = router;
